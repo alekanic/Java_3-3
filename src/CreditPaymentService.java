4267838d7e97e0553c9e81d7e733bde8606c1240
alekanic <koirallabublik@gmail.com>
2023-04-14 12:35:26 +0300

    Second commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,8 @@
 public class CreditPaymentService {
 
     public int calculate(int sum, double procent, int months) {
-        double payment = sum * ((procent * Math.pow(1 + procent, months)) / (Math.pow(1 + procent, months) - 1));
+        double monthProcent = procent / months / 100;
+        double payment = sum * ((monthProcent * Math.pow(1 + monthProcent, months)) / (Math.pow(1 + monthProcent, months) - 1));
         return (int) payment;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,22 @@ public class Main {
     public static void main(String[] args) {
         double procent = 9.99;
         int sum = 1_000_000;
-        int months = 24;
-
-        double month_procent = procent / months / 100;
+        int months = 12;
 
         CreditPaymentService service = new CreditPaymentService();
-        double my_payment = service.calculate(sum, month_procent, months);
-        System.out.println(my_payment);
+        double myPayment = service.calculate(sum, procent, months);
+        System.out.println(myPayment);
+        System.out.println();
+
+        int monthsSecond = 24;
+        CreditPaymentService serviceSecond = new CreditPaymentService();
+        double myPaymentSecond = serviceSecond.calculate(sum, procent, monthsSecond);
+        System.out.println(myPaymentSecond);
+        System.out.println();
+
+        int monthsThird = 36;
+        CreditPaymentService serviceThird = new CreditPaymentService();
+        double myPaymentThird = serviceThird.calculate(sum, procent, monthsThird);
+        System.out.println(myPaymentThird);
     }
 }
