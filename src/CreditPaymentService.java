43cbec142d196af4d7367cdec74a677f8a2c8b5a
alekanic <koirallabublik@gmail.com>
2023-04-18 22:29:24 +0300

    Mistake was detected

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,8 @@
 public class CreditPaymentService {
 
-    public int calculate(int sum, double procent, int months) {
-        double monthProcent = procent / months / 100;
-        double payment = sum * ((monthProcent * Math.pow(1 + monthProcent, months)) / (Math.pow(1 + monthProcent, months) - 1));
+    public int calculate(double sum, double procent, double months) {
+        double monthProcent = procent / 12 / 100.0;
+        double payment = sum * ((monthProcent * Math.pow(1.0 + monthProcent, months)) / (Math.pow(1.0 + monthProcent, months) - 1.0));
         return (int) payment;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         double procent = 9.99;
-        int sum = 1_000_000;
-        int months = 12;
+        double sum = 1_000_000.0;
+        double months = 12.0;
 
         CreditPaymentService service = new CreditPaymentService();
-        double myPayment = service.calculate(sum, procent, months);
+        int myPayment = service.calculate(sum, procent, months);
         System.out.println(myPayment);
         System.out.println();
 
-        int monthsSecond = 24;
+        months = 24.0;
         CreditPaymentService serviceSecond = new CreditPaymentService();
-        double myPaymentSecond = serviceSecond.calculate(sum, procent, monthsSecond);
+        int myPaymentSecond = serviceSecond.calculate(sum, procent, months);
         System.out.println(myPaymentSecond);
         System.out.println();
 
-        int monthsThird = 36;
+        months = 36.0;
         CreditPaymentService serviceThird = new CreditPaymentService();
-        double myPaymentThird = serviceThird.calculate(sum, procent, monthsThird);
+        int myPaymentThird = serviceThird.calculate(sum, procent, months);
         System.out.println(myPaymentThird);
     }
 }
